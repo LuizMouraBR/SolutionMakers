88caa961ea0bfe108aae3665a6f4eda14b757909
Luiz Moura <luiz.philip.mm@gmail.com>
2018-12-06 18:16:52 -0200

    Cadastro de cliente

---
+++ src/main/java/br/edu/fapi/poo/SolutionMakers/cliente/dao/ClienteDAO.java
@@ -0,0 +1,11 @@
+package br.edu.fapi.poo.SolutionMakers.cliente.dao;
+
+import java.util.List;
+
+import br.edu.fapi.poo.SolutionMakers.cliente.model.Cliente;
+
+public interface ClienteDAO {
+
+	public List<Cliente> listarTodos();
+	
+}
---
+++ src/main/java/br/edu/fapi/poo/SolutionMakers/cliente/model/Cliente.java
@@ -0,0 +1,58 @@
+package br.edu.fapi.poo.SolutionMakers.cliente.model;
+
+public class Cliente {
+
+	private int id;
+	private String nomeCompleto;
+	private String rg;
+	private String cpf;
+	private String pis;
+	
+	private int empresaId;
+	private int usuarioId;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getPis() {
+		return pis;
+	}
+	public void setPis(String pis) {
+		this.pis = pis;
+	}
+	public int getEmpresaId() {
+		return empresaId;
+	}
+	public void setEmpresaId(int empresaId) {
+		this.empresaId = empresaId;
+	}
+	public int getUsuarioId() {
+		return usuarioId;
+	}
+	public void setUsuarioId(int usuarioId) {
+		this.usuarioId = usuarioId;
+	}
+
+
+}
